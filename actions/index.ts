77729d301ac1de328ffb292fd74525491cd2202d
emvalencaf <edsonmvf@gmail.com>
2023-06-15 17:40:24 -0300

    create action
    created getLikedSongs action

--- actions/index.ts
+++ actions/index.ts
@@ -1,8 +1,10 @@
+import getLikedSongs from "./getLikedSongs";
 import getSongs from "./getSongs";
 import getSongsByTitle from "./getSongsByTitle";
 import getSongsByUserId from "./getSongsByUserId";
 
 export {
+    getLikedSongs,
     getSongs,
     getSongsByTitle,
     getSongsByUserId,
