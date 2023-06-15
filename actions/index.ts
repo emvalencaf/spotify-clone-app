767bb50a30049066aa0ce4f2beb0b6ad0b7b8d68
emvalencaf <edsonmvf@gmail.com>
2023-06-15 16:26:20 -0300

    create hook and action
    - create getSongsByTitle action
    - create useDebounce hook

--- actions/index.ts
+++ actions/index.ts
@@ -1,7 +1,9 @@
 import getSongs from "./getSongs";
+import getSongsByTitle from "./getSongsByTitle";
 import getSongsByUserId from "./getSongsByUserId";
 
 export {
     getSongs,
+    getSongsByTitle,
     getSongsByUserId,
 }
--- hooks/index.ts
+++ hooks/index.ts
@@ -1,10 +1,12 @@
 import useAuthModal from "./useAuthModal";
+import useDebounce from "./useDebounce";
 import useLoadImage from "./useLoadImage";
 import useUploadModal from "./useUploadModal";
 import useUser from "./useUser";
 
 export {
     useAuthModal,
+    useDebounce,
     useUploadModal,
     useLoadImage,
     useUser,
