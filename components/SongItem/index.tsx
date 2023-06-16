f0b464251be07fd8ac39110fd13df5d88bf6b14a
emvalencaf <edsonmvf@gmail.com>
2023-06-16 15:13:11 -0300

    create api
    create api create-checkout-session;
    create api create-portal-link;

--- components/SongItem/index.tsx
+++ components/SongItem/index.tsx
@@ -22,58 +22,63 @@ const SongItem = ({
         <div
             onClick={() => onClick(data.id)}
             className="
-                relative
-                group
-                flex
-                flex-col
-                items-center
-                justify-center
-                rounded-md
-                overflow-hidden
-                gap-x-4
-                bg-neutral-400/5
-                cursor-pointer
-                hover:bg-neutral-400/100
-                transition
-                p-3
-            "
+          relative 
+          group 
+          flex 
+          flex-col 
+          items-center 
+          justify-center 
+          rounded-md 
+          overflow-hidden 
+          gap-x-4 
+          bg-neutral-400/5 
+          cursor-pointer 
+          hover:bg-neutral-400/10 
+          transition 
+          p-3
+        "
         >
-            <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
+            <div
+                className="
+            relative 
+            aspect-square 
+            w-full
+            h-full 
+            rounded-md 
+            overflow-hidden
+          "
+            >
                 <Image
                     className="object-cover"
-                    src={imagePath || '/assets/liked.png'}
+                    src={imagePath || '/images/music-placeholder.png'}
                     fill
-                    alt="image"
+                    alt="Image"
                 />
             </div>
             <div className="flex flex-col items-start w-full pt-4 gap-y-1">
-                <p
-                    className="font-semibold truncate w-full"
-                >
+                <p className="font-semibold truncate w-full">
                     {data.title}
                 </p>
                 <p
                     className="
-                        text-neutral-400
-                        text-sm
-                        pb-4
-                        w-full
-                        truncate
-                    "
+              text-neutral-400 
+              text-sm 
+              pb-4 
+              w-full 
+              truncate
+            "
                 >
                     By {data.author}
                 </p>
             </div>
             <div
                 className="
-                    absolute
-                    bottom-24
-                    right-5
-                "
+            absolute 
+            bottom-24 
+            right-5
+          "
             >
-                <PlayButton
-                
-                />
+                <PlayButton />
             </div>
         </div>
     );
